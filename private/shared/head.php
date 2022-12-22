8fb3ced4ce20ce1529f14e8a35e6e411bcd70121
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-22 20:46:50 +0200

    Add form elements

--- private/shared/head.php
+++ private/shared/head.php
@@ -5,5 +5,5 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $page_title; ?></title>
-  <link rel="stylesheet" href="../../public/styles/main.css">
+  <link rel="stylesheet" href="./style.css">
 </head>
--- public/new_product.php
+++ public/new_product.php
@@ -3,12 +3,39 @@
 <?php $page_title = 'Product Add'; ?>
 <?php include('../private/shared/head.php'); ?>
 <body>
-  <form action="">
+  <header>
+    <nav>
+      <h1>Product Add</h1>
+      <div id='form-buttons'>
+        <button >Save</button>
+        <a href="./index.php">
+          <button type='button'>Cancel</button>
+        </a>
+      </div>
+    </nav>
+  </header>
+  <form action="" id='product_form' method='POST'>
+    <label for="sku">SKU</label>
+    <input type="text" name="sku" id='sku'>
+    <label for="name">Name</label>
+    <input type="text" name='name' id='name'>
+    <label for="price">Price</label>
+    <input type="number" name='price' id='price'>
     <select name="typeSwitcher" id="productType">
-      <option value="dvd">DVD</option>
-      <option value="book">Book</option>
-      <option value="furniture">Furniture</option>
+      <option value="dvd" id='DVD'>DVD</option>
+      <option value="book" id='Book'>Book</option>
+      <option value="furniture" id='Furniture'>Furniture</option>
     </select>
+    <label for="size">Size (MB)</label>
+    <input type="number" name='size' id='size'>
+    <label for="weight">Weight (KG)</label>
+    <input type="number" name='weight_kg' id='weight'>
+    <label for="height">Height (CM)</label>
+    <input type="number" name='height' id='height'>
+    <label for="width">Width (CM)</label>
+    <input type="number" name='width' id='width'>
+    <label for="length">Length (CM)</label>
+    <input type="number" name='length' id='length'>
   </form>
 <?php include('../private/shared/footer.php'); ?>
 </body>
