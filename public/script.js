f65c9efcb5172c310c40e0d84a1d03594b32c90e
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-23 13:30:12 +0200

    Add dynamic product type change

--- public/script.js
+++ public/script.js
@@ -1,6 +1,22 @@
 $(document).ready(() => {
   $('select').change(() => {
     const VALUE = $('select option:selected');
-    console.log(VALUE.text());
+    const SIZE_CONTAINER = $('#size-container');
+    const WEIGHT_CONTAINER = $('#weight-container');
+    const DIMENSIONS_CONTAINER = $('#dimensions-container');
+
+    if (VALUE.text() === 'Book') {
+      WEIGHT_CONTAINER.show();
+      SIZE_CONTAINER.hide();
+      DIMENSIONS_CONTAINER.hide();
+    } else if (VALUE.text() === 'Furniture') {
+      WEIGHT_CONTAINER.hide();
+      SIZE_CONTAINER.hide();
+      DIMENSIONS_CONTAINER.show();
+    } else {
+      WEIGHT_CONTAINER.hide();
+      SIZE_CONTAINER.show();
+      DIMENSIONS_CONTAINER.hide();
+    }
   });
 });
